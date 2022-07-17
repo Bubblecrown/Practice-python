9cb14068b8962277631f3f0358b2e7f591f5c979
Bubblecrown <suchanya.namwong1@gmail.com>
2022-07-17 20:53:59 +0700

    Commit workshop

---
+++ Variable/Variable.py
@@ -0,0 +1,31 @@
+# 1
+# roti = int(input('Amount of roti: '))
+# price_roti = 30
+# total = roti * price_roti
+# text='บาท'
+# print(f"{total} {text}")
+
+# 2
+# taro = int(input('เผือกทอดจำนวน: '))
+# pork_bread = int(input('ขนมปังหน้าหมูจำนวน: '))
+# taro_price = 30
+# pork_bread_price = 40
+# total = (taro_price * taro) + (pork_bread_price * pork_bread)
+# print(f"{total} บาท")
+
+# No.2
+# x = int(input('Enter your first number:'))
+# y = int(input('Enter your secont number:'))
+# z = (2*x) + (2*y)
+# print(z)
+
+# No. 3 
+# x = int(input('Enter your first number:'))
+# y = 2*(x + 1)
+# print(y)
+
+# No . 7
+x = int(input('Enter your number: '))
+y = x**2 + 2*x + 1
+print(y)
+
